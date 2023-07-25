deb7c65e7cc6fa6d5f501504dbccfbafdfc8e131
Aayush Gupta <guptaaayush432@gmail.com>
2023-07-25 17:07:12 +0530

    [script] added helperConfig

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        // Before startBroadcast -> Not a "real" tx
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+        // anything before startBroadcast will not be send as real transcation.
         vm.startBroadcast();
-        /// sepolia address
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        /// adding price feed sepolia address
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
